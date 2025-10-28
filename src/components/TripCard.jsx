89a1656da376d3aa9f3b2f3c2507fe6d74c54817
roskonenov <roskonenov@gmail.com>
2025-10-28 21:58:59 +0200

    add search trip and sort by rating functionality

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -8,12 +8,25 @@ const TripCard = ({
   description,
   rating
 }) => {
+
+  const stars = Array(5).fill(0);
+
+  const colors = {
+        yellow: "#ffb400",
+        grey: "#797979ff"
+    }
+
   return (
     <div className='trip-card'>
       <img src={image} alt={name} className='trip-image' />
       <div className='trip-content'>
         <h3 className='trip-name'>{name}</h3>
-        <p className='rating'><FaStar className='icon'/> {rating}</p>
+        <p className='rating'>
+          {stars.map((_, index) => (
+              <FaStar className='icon'
+              key={index}
+              color={rating > index ? colors.yellow : colors.grey}/>
+          ))}</p>
         <p className='description'>{description}</p>
         <button>More Info</button>
       </div>
--- src/pages/TripList.jsx
+++ src/pages/TripList.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useFetchTrips from '../hooks/useFetchTrips'
 import TripCard from '../components/TripCard';
 import './TripList.scss'
+import { FaLongArrowAltUp } from "react-icons/fa";
+import { FaLongArrowAltDown } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 const TripList = () => {
-    const {trips, loading, error} = useFetchTrips();
-  return (
-    <div className='container'>
-        <div className='controls'>
-            <input className='search-bar' type="text" placeholder='Search trip...' />
-            <button className='sort-button'>Sort</button>
+    const { trips, loading, error } = useFetchTrips();
+    const [searchQuery, setSearchQuery] = useState('');
+    const [sortByRating, setSortByRating] = useState('');
+
+    const filteredTrips = trips
+        .filter(t => t.name.toLowerCase().includes(searchQuery.toLowerCase()))
+        .sort((a, b) => sortByRating === '' ? 0 : sortByRating === 'asc' ? a.rating - b.rating : b.rating - a.rating);
+
+    return (
+        <div className='container'>
+            <div className='controls'>
+                <input
+                    className='search-bar'
+                    type="text"
+                    placeholder='Search trip...'
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)} />
+                <button
+                    className='sort-button'
+                    value={sortByRating}
+                    onClick={() => setSortByRating(s => s === 'asc' ? 'desc' : 'asc')}>
+                    Sort by rating
+                    {sortByRating === '' ?
+                        <FaStar className='icon' /> :
+                        sortByRating === 'asc' ?
+                            <FaLongArrowAltUp className='icon' /> :
+                            <FaLongArrowAltDown className='icon' />}
+                </button>
+            </div>
+            <section className='trip-list'>
+                {filteredTrips.map((trip) => (
+                    <TripCard
+                        key={trip.id}
+                        name={trip.name}
+                        image={trip.image}
+                        description={trip.description}
+                        rating={trip.rating}
+                    />
+                ))}
+            </section>
         </div>
-        <section className='trip-list'>
-            {trips && trips.map((trip) => (
-                <TripCard 
-                    key={trip.id}
-                    name={trip.name}
-                    image={trip.image}
-                    description={trip.description}
-                    rating={trip.rating}
-                />
-            ))}
-        </section>
-    </div>
-  )
+    )
 }
 
 export default TripList
